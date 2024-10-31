8831849ff6cc2b47d082054ecd57533b2098eacb
Jovonijohnson <Jovenom25@yahoo.com>
2024-10-30 22:48:38 -0700

    added fixes

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let greet = "Welcome2thedungeon"
+function greet(name) {
+    return `Welcome2thedungeon, ${name}!`;
+}
 
-console.log(greet("some name"));
+console.log(greet("some name")); // Outputs: Welcome2thedungeon, some name!
 
 function getCircumference(diameter) { const pi = Math.PI; }
 // Pi constant return pi * diameter;
 
 function getCircumference(diameter) {
-    const pi = Math.PI;
-     // Pi constant
-    return pi * diameter;
-     // Circumference formula: π * diameter
+    const pi = Math.PI; // Pi constant
+    return pi * diameter; // Circumference formula: π * diameter
+}
+
+function firstFunction(diameter) {
+    console.log("Starting first function...");
+
+    // Call to second function
+    let circumference = getCircumference(diameter);
+
+    console.log(`The circumference is: ${circumference}`);
+    console.log("Finished call to second function.");
 }
