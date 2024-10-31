a81d5f59357dc38d3f864ec8b84df6ec75e59934
Jovonijohnson <Jovenom25@yahoo.com>
2024-10-30 22:37:41 -0700

    put in function getCircumference

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ console.log(greet("some name"));
 function getCircumference(diameter) { const pi = Math.PI; }
 // Pi constant return pi * diameter;
 
+function getCircumference(diameter) {
+    const pi = Math.PI;
+     // Pi constant
+    return pi * diameter;
+     // Circumference formula: π * diameter
+}
